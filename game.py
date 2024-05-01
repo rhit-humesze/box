796ebb2cbfad5ce5e8e3abbe63f2349e0782051e
rhit-humesze <78326664+rhit-humesze@users.noreply.github.com>
2024-05-01 17:23:09 -0400

    add game states

--- game.py
+++ game.py
@@ -6,13 +6,14 @@ import string
 
 class Game:
     def __init__(self, width=600, height=400):
-        '''initialize game'''
+        '''initialize Game'''
         pygame.init()
         self.WIDTH:  int = width
         self.HEIGHT: int = height
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.font = pygame.font.SysFont(None, 36)
         pygame.display.set_caption("Box")
+        self.game_state: str = 'start-screen'
 
     def generate_code(self):
         '''generates a random 6-digit alphanumeric code'''
@@ -21,9 +22,8 @@ class Game:
 
     # Main loop
     def game_loop(self):
+        '''main loop for the game screen'''
         running = True
-        start_button_visible = True
-        code_generated = False
         generated_code = None
 
         while running:
@@ -33,23 +33,21 @@ class Game:
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if start_button_visible:
+                    if self.game_state == 'start-screen':
                         mouse_x, mouse_y = pygame.mouse.get_pos()
                         if self.WIDTH / 2 - 50 <= mouse_x <= self.WIDTH / 2 + 50 and self.HEIGHT / 2 - 25 <= mouse_y <= self.HEIGHT / 2 + 25:
                             generated_code = self.generate_code()
-                            start_button_visible = False
-                            code_generated = True
+                            self.game_state = 'code-screen'
                     else:
-                        # Do something else when code is generated (if needed)
+                        #for future
                         pass
 
-            if start_button_visible:
-                # Display "START" button
+            if self.game_state == 'start-screen':
                 pygame.draw.rect(self.screen, pygame.Color('black'), (self.WIDTH / 2 - 50, self.HEIGHT / 2 - 25, 100, 50))
                 start_text = self.font.render("START", True, pygame.Color('white'))
                 start_rect = start_text.get_rect(center=(self.WIDTH // 2, self.HEIGHT // 2))
                 self.screen.blit(start_text, start_rect)
-            elif code_generated:
+            elif self.game_state == 'code-screen':
                 # Display generated code
                 text = self.font.render("Enter code: " + generated_code, True, pygame.Color('black'))
                 text_rect = text.get_rect(center=(self.WIDTH // 2, 50))
