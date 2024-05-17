a9eec98ae4ebf2cf802c34e553477674606ecdc2
rhit-humesze <78326664+rhit-humesze@users.noreply.github.com>
2024-05-17 01:14:09 -0400

    gets messages

--- host.py
+++ host.py
@@ -1,10 +1,8 @@
 import eventlet
 import socketio
-import threading
 import base64
 import os
 from models import DrawingData
-
 from queue import Queue
 
 class Host:
@@ -15,13 +13,13 @@ class Host:
         self.gameCode = code
         self.server = None
 
-        #outtake queue
+        # outtake queue
         self.join_q = join_q
         self.disc_q = disc_q
         self.draw_q = draw_q
         self.vote_q = vote_q
 
-        #intake queue
+        # intake queue
         self.msg_q = msg_q
         
         # init events
@@ -33,28 +31,24 @@ class Host:
         self.sio.on("drawingVote", self.drawingVote)
         
     def run(self):
-        '''start the server'''
-        serv_thread = threading.Thread(target=self.start_server)
-        serv_thread.start()
-
-        msg_handler_thread = threading.Thread(target=self.server_message_handler)
-        msg_handler_thread.start()
-        # serv_thread.join()
-        # self.start_server()
+        '''start the server and the message handler in the same thread'''
+        eventlet.spawn(self.server_message_handler)
+        self.start_server()
     
     def server_message_handler(self):
         while True:
             if not self.msg_q.empty():
                 msg = self.msg_q.get()
-                if(msg == 0):
+                if msg == 0:
                     self.sio.emit("drawSomeDraw")
                     print("\nstarting drawing round\n")
-                if(msg == 1):
+                elif msg == 1:
                     self.sio.emit("timesUp")
                     print("\nTIMES UP!!!\n")
-                if(msg == 2):
+                elif msg == 2:
                     self.sio.emit("drawSomeVote")
                     print("\nstarting voting round\n")
+            eventlet.sleep(0.1)  # Sleep briefly to prevent a tight loop
 
     def start_server(self):
         self.server = eventlet.wrap_ssl(eventlet.listen(('', 5100)), 
@@ -80,7 +74,7 @@ class Host:
 
     def checkCode(self, sid, data):
         print(f"Received game code {data} from connected client {sid}.")
-        if(data == self.gameCode):
+        if data == self.gameCode:
             self.sio.emit("codeAccepted", room=sid)
             print(f"Client {sid} code accepted.")
             self.printServerInfo()
@@ -91,7 +85,7 @@ class Host:
     def newUser(self, sid, data):
         print(f"Client {sid} player name set to {data}.")
         self.players[sid] = data
-        self.join_q.put({sid:data})
+        self.join_q.put({sid: data})
         self.printServerInfo()
         
     def drawingSubmission(self, sid, imageData, name):
@@ -102,13 +96,13 @@ class Host:
         with open(path, "wb") as fh:
             fh.write(base64.decodebytes(img_data))
         drawing = DrawingData(filepath, name, self.players[sid])
-        self.draw_q.put({sid:drawing})
+        self.draw_q.put({sid: drawing})
     
     def drawingVote(self, sid, side):
-        if(side == "left"):
-            self.vote_q.put({sid:int(0)})
+        if side == "left":
+            self.vote_q.put({sid: int(0)})
         else:
-            self.vote_q.put({sid:int(1)})
+            self.vote_q.put({sid: int(1)})
     
     def stop(self):
         '''safely stop the server'''
